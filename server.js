0b123024c815ed94b71eb8aa8f9773b7d1b458b5
Nick <javokhirru03@gmail.com>
2025-09-17 18:06:41 +0900

    feat: initia; set up

---
+++ server.js
@@ -0,0 +1,34 @@
+console.log("Running the web server");
+const { setDefaultResultOrder } = require("dns");
+const express = require("express");
+const app = express();
+const http = require("http");
+
+
+// 1 Kirish code
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 2: Session code
+
+// 3 Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 4 Routing
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "Success" });
+});
+app.get("/", function (req, res) {
+  res.render("harid");
+});
+
+const server = http.createServer(app);
+let PORT = 3000;
+
+server.listen(PORT, function () {
+  console.log(
+    `The server is running successfully on PORT: http://localhost:${PORT}`);
+});
