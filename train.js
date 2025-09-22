e8f13c3814d3c5ecbf612d07567100ca63884f0b
Nick <javokhirru03@gmail.com>
2025-09-22 17:33:01 +0900

    feat: train callback va asynchronous function

--- train.js
+++ train.js
@@ -1,1 +1,84 @@
-//hrfth
+// const list = [
+//     "Yahshi talaba boling", // 0-20
+//     "Togri boshliq tanlang va koproq hato qiling", // 20-30
+//     "Uzingnizga ishlashni boshang", // 30-40
+//     "Siz kuchli bolgan narsalarni qiling", // 40-50
+//     "Yoshlarga investitsiya qiling", // 50-60
+//     "Endi dam oling", // 60+
+// ];
+
+// function maslahatBerish(yosh, callback) {
+//     if (typeof yosh !== "number") callback("raqam kiriting", null);
+//     else if (yosh <= 20) callback(null, list[0]);
+//     else if (yosh > 20 && yosh <= 30) callback(null, list[1]);
+//     else if (yosh > 30 && yosh <= 40) callback(null, list[2]);
+//     else if (yosh > 40 && yosh <= 50) callback(null, list[3]);
+//     else if (yosh > 50 && yosh <= 60) callback(null, list[4]);
+//     else {
+//         setTimeout(function () {
+//             callback(null, list[5]);
+//         }, 5000);
+//     }
+// }
+
+// console.log("passed here 1111");
+
+// maslahatBerish(65, (err, data) => {
+//     if (err) console.log("Error:", err);
+//     else {
+//         console.log("Javob:", data);
+//     }
+// });
+
+// console.log("passed here 2222");
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+const list = [
+    "Yahshi talaba boling", // 0-20
+    "Togri boshliq tanlang va koproq hato qiling", // 20-30
+    "Uzingnizga ishlashni boshang", // 30-40
+    "Siz kuchli bolgan narsalarni qiling", // 40-50
+    "Yoshlarga investitsiya qiling", // 50-60
+    "Endi dam oling", // 60+
+];
+
+async function maslahatBerish(yosh) {
+    if (typeof yosh !== "number") throw new Error ("raqam kiriting");
+    else if (yosh <= 20) return list[0];
+    else if (yosh > 20 && yosh <= 30) return list[1];
+    else if (yosh > 30 && yosh <= 40) return list[2];
+    else if (yosh > 40 && yosh <= 50) return list[3];
+    else if (yosh > 50 && yosh <= 60) return list[4];
+    else {
+        return new Promise((resolve, reject) => {
+            setTimeout(function () {
+                resolve(list[5]);
+            }, 5000);
+        });
+    }
+}
+
+// console.log("passed here 1111");
+
+// maslahatBerish(25)
+// .then((data) => {
+//     console.log("Javob:", data);
+// })
+// .catch((err) => {
+//     console.log("Error:", err);
+// });
+
+// console.log("passed here 2222");
+
+
+async function run() {
+    let javob = await maslahatBerish(100);
+    console.log("Javob:", javob);
+    javob = await maslahatBerish(25);
+    console.log("Javob:", javob);   
+    javob = await maslahatBerish(10);
+    console.log("Javob:", javob);   
+}
+run();
